7ea3f4891576b2e83199fce54aa685266d3d13e9
DESKTOP-GJMMA2C\Entronica_nut <nattaporn.k@entronica.co.th>
2017-11-03 16:05:39 +0700

    91. Building a Structural Directive

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -6,12 +6,14 @@ import { HttpModule } from '@angular/http';
 import { AppComponent } from './app.component';
 import { BasicHighlightDirective } from 'app/basic-highlight/basic-highlight.directive';
 import { BetterHighlightDirective } from 'app/better-highlight/better-highlight.directive';
+import { UnlessDirective } from './unless.directive';
 
 @NgModule({
   declarations: [
     AppComponent,
     BasicHighlightDirective,
-    BetterHighlightDirective
+    BetterHighlightDirective,
+    UnlessDirective
   ],
   imports: [
     BrowserModule,
